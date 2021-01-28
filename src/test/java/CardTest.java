bdbcfcc146e7296f6eda8901f09569b993eb566e
Laura Dobie <moving.toyshop@gmail.com>
2021-01-28 14:43:56 +0000

    Card class setup complete.

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CardTest {
+
+    Card card;
+
+    @Before
+    public void before() {
+        card = new Card(SuitType.SPADES, RankType.ACE);
+    }
+
+    @Test
+    public void canGetSuit() {
+        assertEquals(SuitType.SPADES, card.getSuit());
+    }
+
+    @Test
+    public void canGetRank() {
+        assertEquals(RankType.ACE, card.getRank());
+    }
+
+    @Test
+    public void canGetValue() {
+        assertEquals(1, card.getCardValue());
+    }
 }
